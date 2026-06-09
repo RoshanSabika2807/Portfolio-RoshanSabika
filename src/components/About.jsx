3dba77743cebc1beee6f625a3c64233ccf255fde
RoshanSabika2807 <roshansabika2807@gmail.com>
2026-06-09 17:05:15 +0530

    sets screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ function About() {
         whileInView={{ opacity: 1 }}
         viewport={{ amount: 0.2 }}
         transition={{ duration: 0.8 }}
-        className="text-5xl font-bold mb-10 text-left"
+        className="text-5xl sm:text-4xl font-bold mb-10 text-left"
       >
         ABOUT
       </motion.h2>
@@ -27,7 +27,7 @@ function About() {
         }}
         className="max-w-3xl bg-[#5684AE]  shadow-lg rounded-xl p-8 border-l-4 border-[#5A7D6E] cursor-pointer transition-all duration-300"
       >
-        <p className="mb-4 text-gray-900 leading-8 text-left">
+        <p className="mb-4 sm:text-4xl-gray-900 leading-8 text-left">
           I am a BCA student with a strong passion for web development,
           focused on building modern, responsive, and intuitive digital
           experiences. I enjoy transforming ideas into functional web
@@ -40,7 +40,7 @@ function About() {
           impactful, real-world solutions through technology.
         </p>
 
-        <p className="text-gray-900 leading-8 text-left">
+        <p className="sm:text-4xl-gray-900 leading-8 text-left">
           Alongside my academics, I actively work on improving my practical
           skills by building small projects and experimenting with different
           design and development concepts.
