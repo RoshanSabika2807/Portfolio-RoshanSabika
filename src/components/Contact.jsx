ea3a460ccafb91cd100044b1c3f92e5846c969c5
RoshanSabika2807 <roshansabika2807@gmail.com>
2026-06-09 15:41:35 +0530

    updated portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,21 +5,22 @@ function Contact() {
       <h2 className="text-4xl font-bold mb-6">
         Contact 
       </h2>
-
-      <p>
-        Email:{" "}
-        <a href="mailto:roshansabika2807@gmail.com" className="text-blue-600 underline">
-          roshansabika2807@gmail.com
-        </a>
-      </p>
-
+      
       <a
-        href="https://github.com/yourusername"
+        href="https://github.com/RoshanSabika2807"
         target="_blank"
+        rel="noopener noreferrer"
         className="inline-block mt-5 bg-[#645394] text-white px-6 py-3 rounded-lg"
       >
         GitHub
       </a>
+
+      
+        Email:{" "}
+        <a href="mailto:roshansabika2807@gmail.com" className="text-blue-600 underline">
+          roshansabika2807@gmail.com
+        </a>
+      
 
       <p className="mt-6">
         Let's connect and create something meaningful!
