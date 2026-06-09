bdacceec1c387fe7eb7f1e9559151ebd060ff93a
RoshanSabika2807 <roshansabika2807@gmail.com>
2026-06-09 18:10:39 +0530

    new scaling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,9 +25,9 @@ function About() {
           scale: 1.03,
           y: -8
         }}
-        className="max-w-3xl bg-[#5684AE]  shadow-lg rounded-xl p-8 border-l-4 border-[#5A7D6E] cursor-pointer transition-all duration-300"
+        className="w-full max-w-3xl bg-[#5684AE]  shadow-lg rounded-xl p-4 md:p-8 border-l-4 border-[#5A7D6E] cursor-pointer transition-all duration-300"
       >
-        <p className="mb-4 text-gray-900 leading-8 text-left">
+        <p className="mb-4 text-gray-900 text-sm md:text-base leading-7 md:leading-8 text-left">
           I am a BCA student with a strong passion for web development,
           focused on building modern, responsive, and intuitive digital
           experiences. I enjoy transforming ideas into functional web
@@ -40,7 +40,7 @@ function About() {
           impactful, real-world solutions through technology.
         </p>
 
-        <p className="text-gray-900 leading-8 text-left">
+        <p className="text-gray-900 text-sm md:text-base leading-7 md:leading-8 text-left">
           Alongside my academics, I actively work on improving my practical
           skills by building small projects and experimenting with different
           design and development concepts.
